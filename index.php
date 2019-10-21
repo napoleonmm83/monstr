e04614a9b315ba936025540205ae973e032eb3fa
napoleonmm83 <marcusmartini83@gmail.com>
2019-10-21 10:35:38 +0200

    mainbrancg

--- index.php
+++ index.php
@@ -4,17 +4,62 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <style>
 
+
+
+      </style>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <div class="container-fluid">
-      ...
+
+    <div class="container-fluid ">
+      <div class="row" style="height:6vh;"  >
+    <div class="col-sm border">
+      <h1 class="text-center" >Montag</h1>
     </div>
+    <div class="col-sm border">
+    <h1 class="text-center" > Dienstag</h1>
+    </div>
+    <div class="col-sm border">
+      <h1 class="text-center" >Mittwoch</h1>
+    </div>
+    <div class="col-sm border">
+    <h1 class="text-center" >  Donnerstag</h1>
+    </div>
+    <div class="col-sm border">
+    <h1 class="text-center" >  Freitag</h1>
+    </div>
+  </div>
+
+
+  <div style="height:86vh;"class="row">
+<div  class="col-sm border ">
+<p>montag</p>
+</div>
+<div class="col-sm border ">
+  Dienstag
+</div>
+<div class="col-sm border  ">
+  Mittwoch
+</div>
+<div class="col-sm border  ">
+  Donnerstag
+</div>
+<div class="col-sm border ">
+  Freitag
+</div>
+</div>
+    </div>
+
+    <footer id="sticky-footer" style="height:8vh;" class="py-4 bg-dark text-white-50">
+        <div class="container text-center">
+          <small>Copyright &copy; Your Website</small>
+        </div>
+      </footer>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
